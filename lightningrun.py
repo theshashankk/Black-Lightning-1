b502030533f9b7fbac35f8c7a31cf6960dfa4e0d
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2021-11-20 11:06:54 +0530

    trying to fix

--- lightningrun.py
+++ lightningrun.py
@@ -1,2 +1,3 @@
 import os
-os.system("git clone https://github.com/KeinShin/Black-Lightning /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system('git clone https://github.com/KeinShin/Black-Lightning.git && cd Black-Lightning')
+os.system('python3 -m userbot')
